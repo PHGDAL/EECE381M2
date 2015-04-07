d488d5c1b75d1a910ed9953de06cd9c28e66b9e8
Laeveteinn <rico_wen@hotmail.com>
2015-04-06 19:04:33 -0700

    updated to use two servos, communication skeleton

--- DE2Servo/de2servo.vhd
+++ DE2Servo/de2servo.vhd
@@ -5,55 +5,78 @@ use IEEE.NUMERIC_STD.ALL;
 entity de2servo is
     PORT(
 		  CLOCK_50	: IN STD_LOGIC;
-		  SW			: IN STD_LOGIC_VECTOR(7 downto 0);
+		  SW			: IN STD_LOGIC_VECTOR(15 downto 0);
 		  KEY			: IN STD_LOGIC_VECTOR(3 downto 0);
-        GPIO_1		: OUT STD_LOGIC_VECTOR(2 downto 0);
-		  LEDG		: OUT STD_LOGIC_VECTOR(7 downto 0);
-		  LEDR		: OUT STD_LOGIC_VECTOR(7 downto 0)		  
+        GPIO_1		: INOUT STD_LOGIC_VECTOR(4 downto 0);
+		  LEDG		: OUT STD_LOGIC_VECTOR(2 downto 0)
     );
 end de2servo;
 
 architecture Behavioral of de2servo is
-    COMPONENT servoclk
+    COMPONENT servolinked
         PORT(
-            clk    : in  STD_LOGIC;
-            reset  : in  STD_LOGIC;
-            clk_out: out STD_LOGIC
+            clk    : in STD_LOGIC;
+            reset  : in STD_LOGIC;
+            pos	 : in STD_LOGIC_VECTOR(7 downto 0);
+				servo	 : out STD_LOGIC
         );
     END COMPONENT;
     
-    COMPONENT servopwm
-        PORT (
-            clk   : IN  STD_LOGIC;
-            reset : IN  STD_LOGIC;
-            pos   : IN  STD_LOGIC_VECTOR(7 downto 0);
-            servo : OUT STD_LOGIC
-        );
-    END COMPONENT;
-    
-    signal clk_out : STD_LOGIC := '0';
-	 signal pos		 : STD_LOGIC_VECTOR(7 downto 0) := "00000000";
+    signal SW1	: STD_LOGIC_VECTOR(7 downto 0);
+	 signal SW2 : STD_LOGIC_VECTOR(7 downto 0);
+	 signal coordx : STD_LOGIC_VECTOR(7 downto 0);
+	 signal coordy : STD_LOGIC_VECTOR(7 downto 0);
 begin
-    servoclk_map: servoclk PORT MAP(
+    servoVertical: servolinked PORT MAP(
         clk => CLOCK_50,
 		  reset => not KEY(3),
-		  clk_out => clk_out
-    );
-    
-    servopwm_map: servopwm PORT MAP(
-        clk => clk_out,
-		  reset => not KEY(3),
-		  pos => pos,
+		  pos => SW1,
 		  servo => GPIO_1(0)
+    );
+	 
+    servoHorizontal: servolinked PORT MAP(
+        clk => CLOCK_50,
+		  reset => not KEY(3),
+		  pos => SW2,
+		  servo => GPIO_1(1)
     );
 	 
 	 process(CLOCK_50)
 	 begin
 		if(rising_edge(CLOCK_50)) then
-			if(unsigned(SW(7 downto 0)) < 80) then
-				pos <= SW(7 downto 0);
-				LEDR <= SW;
-			end if;
+		--	 if(unsigned(SW(7 downto 0)) < 80) then
+		--		SW1 <= SW(7 downto 0);
+		--	 end if;
+			 
+		--	 if(unsigned(SW(15 downto 8)) < 80) then
+		--		SW2 <= SW(15 downto 8);
+		--	 end if;		
 		end if;
 	 end process;
+	 
+	 process(GPIO_1(2))
+	 begin
+		if(rising_edge(GPIO_1(2))) then
+		-- Update X coordinate
+			coordx(0) <= GPIO_1(3);
+			coordx(1) <= coordx(0);
+			coordx(2) <= coordx(1);
+			coordx(3) <= coordx(2);
+			coordx(4) <= coordx(3);
+			coordx(5) <= coordx(4);
+			coordx(6) <= coordx(5);
+			coordx(7) <= coordx(6);
+		-- Update Y coordinate
+			coordy(0) <= GPIO_1(4);
+			coordy(1) <= coordy(0);
+			coordy(2) <= coordy(1);
+			coordy(3) <= coordy(2);
+			coordy(4) <= coordy(3);
+			coordy(5) <= coordy(4);
+			coordy(6) <= coordy(5);
+			coordy(7) <= coordy(6);
+		end if;
+	 end process;
+	 
+	 
 end Behavioral;
---
+++ DE2Servo/servolinked.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity servolinked is
+    PORT(
+		  clk			: IN STD_LOGIC;
+		  pos			: IN STD_LOGIC_VECTOR(7 downto 0);
+		  reset		: IN STD_LOGIC;
+        servo		: OUT STD_LOGIC	  
+    );
+end servolinked;
+
+architecture Behavioral of servolinked is
+    COMPONENT servoclk
+        PORT(
+            clk    : in  STD_LOGIC;
+            reset  : in  STD_LOGIC;
+            clk_out: out STD_LOGIC
+        );
+    END COMPONENT;
+    
+    COMPONENT servopwm
+        PORT (
+            clk   : IN  STD_LOGIC;
+            reset : IN  STD_LOGIC;
+            pos   : IN  STD_LOGIC_VECTOR(7 downto 0);
+            servo : OUT STD_LOGIC
+        );
+    END COMPONENT;
+    
+    signal clk_out : STD_LOGIC := '0';
+begin
+    servoclk_map: servoclk PORT MAP(
+        clk => clk,
+		  reset => reset,
+		  clk_out => clk_out
+    );
+    
+    servopwm_map: servopwm PORT MAP(
+        clk => clk_out,
+		  reset => reset,
+		  pos => pos,
+		  servo => servo
+    );
+end Behavioral;
