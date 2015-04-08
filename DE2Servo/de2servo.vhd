c9c616086cb9d197aa0086deb6b4ca81db923696
Laeveteinn <rico_wen@hotmail.com>
2015-04-07 18:16:14 -0700

    update to de2pi servo communication

--- DE2Servo/de2servo.vhd
+++ DE2Servo/de2servo.vhd
@@ -7,7 +7,7 @@ entity de2servo is
 		  CLOCK_50	: IN STD_LOGIC;
 		  SW			: IN STD_LOGIC_VECTOR(15 downto 0);
 		  KEY			: IN STD_LOGIC_VECTOR(3 downto 0);
-        GPIO_1		: INOUT STD_LOGIC_VECTOR(4 downto 0);
+        GPIO_1		: INOUT STD_LOGIC_VECTOR(5 downto 0);
 		  LEDG		: OUT STD_LOGIC_VECTOR(2 downto 0)
     );
 end de2servo;
@@ -22,35 +22,84 @@ architecture Behavioral of de2servo is
         );
     END COMPONENT;
     
-    signal SW1	: STD_LOGIC_VECTOR(7 downto 0);
-	 signal SW2 : STD_LOGIC_VECTOR(7 downto 0);
+    signal Vpos	: STD_LOGIC_VECTOR(7 downto 0);
+	 signal Hpos	: STD_LOGIC_VECTOR(7 downto 0);
 	 signal coordx : STD_LOGIC_VECTOR(7 downto 0);
 	 signal coordy : STD_LOGIC_VECTOR(7 downto 0);
+	 signal Abbey	: STD_LOGIC;
+	 
+	 type t_state is (idle, ready);
 begin
     servoVertical: servolinked PORT MAP(
         clk => CLOCK_50,
 		  reset => not KEY(3),
-		  pos => SW1,
+		  pos => Vpos,
 		  servo => GPIO_1(0)
     );
 	 
     servoHorizontal: servolinked PORT MAP(
         clk => CLOCK_50,
 		  reset => not KEY(3),
-		  pos => SW2,
+		  pos => Hpos,
 		  servo => GPIO_1(1)
     );
 	 
 	 process(CLOCK_50)
+	 variable current_state :	t_state	:= idle;
+	 variable next_state		:	t_state	:= idle;
+	 variable varX				:	unsigned(7 downto 0);
+	 variable varY				:	unsigned(7 downto 0);
 	 begin
-		if(rising_edge(CLOCK_50)) then
-		--	 if(unsigned(SW(7 downto 0)) < 80) then
-		--		SW1 <= SW(7 downto 0);
-		--	 end if;
-			 
-		--	 if(unsigned(SW(15 downto 8)) < 80) then
-		--		SW2 <= SW(15 downto 8);
-		--	 end if;		
+		if(rising_edge(CLOCK_50)) then		
+			case current_state is
+			
+			when idle =>
+				if(GPIO_1(2) = '1') then
+					Abbey <= '1';
+				end if;
+			
+				if(GPIO_1(5) = '1' and Abbey = '1') then
+					next_state := ready;
+				else
+					next_state := idle;
+				end if;			
+				
+			when ready =>
+				varX := unsigned(coordx);
+				varY := unsigned(coordy);
+				if(varX = 0 and varY = 0) then
+				-- Idle
+				else
+					if(varX > 160) then
+					-- Move Right
+						if(unsigned(Hpos) < 80) then
+							Hpos <= std_logic_vector(unsigned(Hpos) + 1);
+						end if;
+					elsif(varX < 140) then
+					-- Move Left
+						if(unsigned(Hpos) < 80) then
+							Hpos <= std_logic_vector(unsigned(Hpos) - 1);
+						end if;
+					end if;
+					
+					if(varY < 90) then
+					-- Move Down
+						if(unsigned(Vpos) < 80) then
+							Vpos <= std_logic_vector(unsigned(Vpos) - 1);
+						end if;
+					end if;
+					if(varY > 110) then
+					-- Move Up
+						if(unsigned(Vpos) < 80) then
+							Vpos <= std_logic_vector(unsigned(Vpos) + 1);
+						end if;
+					end if;
+				end if;
+				Abbey <= '0';
+				next_state := idle;
+			end case;
+			current_state := next_state;
+			
 		end if;
 	 end process;
 	 
